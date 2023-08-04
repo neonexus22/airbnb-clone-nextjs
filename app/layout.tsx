639b0281df8ac949f721e3d25778807ab39dc058
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-08-04 21:07:38 +0545

    initial setup with register

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AirBnB",
+  description: "AirBnB Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
